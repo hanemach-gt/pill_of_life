857ccfd8c97b77b4f06cff60176ebe4e46189b3d
PiotrGry <piotrgryzlo@gmail.com>
2017-09-08 06:15:01 +0200

    levels done

--- hall_of_fame.py
+++ hall_of_fame.py
@@ -1,16 +1,17 @@
 import csv
 
 def write_heigh_score(protagonist_name, exp, defeated_enemy, victory_time = 0, file_name = "hall_of_fame.txt"):
-    with open (file_name, "w", newline="") as new_file:
-        points = int(exp)+ int(defeated_enemy)
-        points = str(points)
-        values = [protagonist_name, exp, defeated_enemy, victory_time]
-        headers = ["player_name","expierience", "defeated_enemy","victory_time"]
-        writer = csv.writer(new_file)
+    f = open(file_name, "w")
 
-        writer.writerow(["|{:^25}|{:^25}|{:^25}|{:^25}|{:^25}|".format
+        f.write(["|{:^25}|{:^25}|{:^25}|{:^25}|{:^25}|".format
                         ("player_name", "expierience", "defeated_enemy", "points", "victory_time")
                         ])
+with open (file_name, "w", newline="") as new_file:
+    points = int(exp)+ int(defeated_enemy)
+    points = str(points)
+    values = [protagonist_name, exp, defeated_enemy, victory_time]
+    # headers = ["player_name","expierience", "defeated_enemy","victory_time"]
+    writer = csv.writer(new_file)
 
         writer.writerow(["|{:^25}|{:^25}|{:^25}|{:^25}|{:^25}|".format
                         (protagonist_name, exp, defeated_enemy, points, victory_time)
